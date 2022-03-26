69d618df9e74f572f89736c7e4ea62140d628a8f
alamuramasamy9 <alamuramasamy12@gmail.com>
2022-03-26 17:24:17 -0400

    a6 React.js sp22

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import {Link, Route, Routes} from "react-router-dom";
 import ExploreScreen from "./ExploreScreen/ExploreScreen";
-import HelloWorld from "../hello-world";
-import Labs from "../labs/labs";
-import HomeScreen from "./HomeScreen/HomeScreen";
 
 const Tuiter = () => {
     return (
