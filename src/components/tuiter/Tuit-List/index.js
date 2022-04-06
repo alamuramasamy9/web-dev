92ad19475d711129a0c41d5afc3a395c4f7c3370
alamuramasamy9 <alamuramasamy12@gmail.com>
2022-04-06 17:14:30 -0400

    a8 REST sp22

--- src/components/tuiter/Tuit-List/index.js
+++ src/components/tuiter/Tuit-List/index.js
@@ -1,20 +1,37 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector}
+    from "react-redux";
+import {findAllTuits, deleteTuit}
+    from "../../../actions/tuits-actions";
+
+
 import TuitListItem from "./tuit-list-item";
+
+
 
 const TuitList = () => {
 
     const selectAllTuits = (state) => state.tuits.tuits;
     const tuits = useSelector(selectAllTuits);
 
+    const dispatch = useDispatch();
+    useEffect(() =>
+            findAllTuits(dispatch),
+        []);
+
+
+
     return (
+
         <ul className="list-group">
             {
                 tuits.map(tuit =>
                     <TuitListItem tuit={tuit} />
+
                 )
             }
         </ul>
+
     )
 };
 
--- src/components/tuiter/reducers/tuit-reducer.js
+++ src/components/tuiter/reducers/tuit-reducer.js
@@ -1,62 +1,14 @@
-import posts from '../data/tuits.json';
-
-const initialState = {
-    tuits: posts
-}
-
-const tuitReducer = (state = initialState, action) => {
+import {DELETE_TUIT, FIND_ALL_TUITS}
+    from "../../../actions/tuits-actions";
+const tuitsReducer = (state = [], action) => {
     switch (action.type) {
-
-        case 'like-tuit':
-            return ({
-                tuits: state.tuits.map(tuit => {
-                    if (tuit._id === action.tuit._id) {
-                        if (tuit.liked === true) {
-                            tuit.liked = false;
-                            tuit.stats.likes--;
-                        } else {
-                            tuit.liked = true;
-                            tuit.stats.likes++;
-                        }
-                        return tuit;
-                    } else {
-                        return tuit;
-                    }
-                })
-            });
-
-        case 'delete-tuit':
-            return ({
-                tuits: state.tuits.filter(tuit => tuit._id !== action.tuit._id)
-            })
-
-        case 'create-tuit':
-            const tuit = {
-                _id: (new Date()).getTime() + '',
-                "topic": "Web Development",
-                "userName": "Alamu Ramasamy",
-                "verified": false,
-                "handle": "al_ra",
-                "time": "now",
-                ...action.tuit,
-                "avatar-image": "/images/profile.jpg",
-                "logo-image": "/images/profile.jpg",
-                "stats": {
-                    "comments": 21,
-                    "retweets": 22,
-                    "likes": 23
-                },
-            };
-            return ({
-                tuits: [
-                    tuit,
-                    ...state.tuits,
-                ]
-            });
-
+        case FIND_ALL_TUITS:
+            return action.tuits;
+        case DELETE_TUIT:
+            return state.filter(
+                tuit => tuit._id !== action.tuit._id);
         default:
-            return (state);
+            return state;
     }
-};
-
-export default tuitReducer;
+}
+export default tuitsReducer;
