4f69c3a46d273cbf939b8b89d13898d37765f54e
alamuramasamy9 <alamuramasamy12@gmail.com>
2022-05-01 15:32:57 -0500

    final fingers crossed

--- full-stack-developer-server-node/controllers/tuits/tuits-schema.js
+++ full-stack-developer-server-node/controllers/tuits/tuits-schema.js
@@ -2,8 +2,21 @@ import mongoose from 'mongoose';
 const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
-  postedBy: {
-    username: String
-  }
+  topic: String,
+  username: String,
+  verified : Boolean,
+  time : String,
+  title : String,
+  tuit : String,
+  handle: String,
+  attachments : {
+    video : String
+  },
+  logoImage : String,
+  avatarImage : String,
+  likes: Number,
+  dislikes: Number,
+  comments: Number,
+  retweets: Number
 }, {collection: 'tuits'});
 export default schema;
--- src/services/tuits-services.js
+++ src/services/tuits-services.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-const TUITS_API = 'https://web-dev-server1.herokuapp.com/api/tuits'
+//const TUITS_API = 'https://web-dev-server1.herokuapp.com/api/tuits'
+const TUITS_API = 'https://a9--web-dev-server1.herokuapp.com/api/tuits'
 
-const TUITS_API = 'http://localhost:4000/api/tuits'
+//const TUITS_API = 'http://localhost:4000/api/tuits'
 export const createTuit = async (tuit) => {
  const response = await axios.post(TUITS_API, tuit)
  return response.data;
